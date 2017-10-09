82a873bdf9c93bbec7a6d0861f36f2faa97a1a03
Qsior <lukaszbojes@gmail.com>
2017-10-09 19:44:19 +0200

    Added examples and exercise for boolean and char primitives

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,14 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
+        char myChar = 'a';
+        char uniChar = '\u00A9';
+        System.out.println(uniChar);
 
+        boolean myBoolean = true;
+
+        //exercise
+        char newUniChar = '\u00AE';
+        System.out.println(newUniChar);
     }
 }
